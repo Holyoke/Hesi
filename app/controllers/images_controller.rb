88dbf243993776e998900d0cf7f40f519a7db81b
Holyoke <kid.idioteque@gmail.com>
2014-11-26 10:24:58 -0800

    Work on Images Controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,25 @@
 class ImagesController < ApplicationController
+  before_action :require_authentication
+  
+  #create image
+  def new 
+    
+  end
+  
+  def create
+  end
+  
+  #read
+  def show
+  end
+  
+  def index
+  end
+  
+  #update, not yet
+  
+  #destroy 
+  def destroy
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126173501) do
+ActiveRecord::Schema.define(version: 20141126180824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "images", force: true do |t|
-    t.integer  "user_id",     null: false
+    t.integer  "user_id",            null: false
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "images", ["user_id"], name: "index_images_on_user_id", using: :btree
