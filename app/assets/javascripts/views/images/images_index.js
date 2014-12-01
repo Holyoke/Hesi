a46646a2a5b2645801cf4bfba29a3ca4c3f97d96
Holyoke <kid.idioteque@gmail.com>
2014-11-30 21:54:43 -0800

    Work on showing user's images

--- app/assets/javascripts/views/images/images_index.js
+++ app/assets/javascripts/views/images/images_index.js
@@ -1,6 +1,20 @@
 Hesigram.Views.ImagesIndex = Backbone.View.extend({
+	
+	initialize: function() {
+		//add event listeners here
+	},
 
   template: JST['images/index'],
+	
+	render: function () {
+		var content = this.template({
+			images: this.collection
+		});
+		
+		this.$el.html(content);
+		
+		return this;
+	},
 	
 	
 
